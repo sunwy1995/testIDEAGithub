e05846a23303db3707a2515a342f054360c49523
sunwy1995-2 <sunwy1995-2@gmail.com>
2021-02-03 22:33:15 +0800

    changed by hot_fix 2

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("current branch is hot_fix");
+        System.out.println("current branch is hot_fix 2");
     }
 }
