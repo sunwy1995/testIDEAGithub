a915aca435e6e9771a8d30a201aae32ef60dbb79
sunwy1995-2 <sunwy1995-2@gmail.com>
2021-02-03 22:35:33 +0800

    changed by hot fix 3

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("current branch is hot_fix");
         System.out.println("current branch is hot_fix 2");
+        System.out.println("current branch is hot_fix 3");
     }
 }
---
+++ HelloGit/src/test/java/com/atguigu/git/TestHelloGit.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+import org.junit.Test;
+
+/**
+ * @author WayneSun
+ * @create 2021-02-03 22:25
+ * @description
+ */
+public class TestHelloGit {
+    @Test
+    public void testHelloGit(){
+
+    }
+}
