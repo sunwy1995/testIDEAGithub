ce21f9ec0287739d6f3f44c121f229544b78544f
sunwy1995-2 <sunwy1995-2@gmail.com>
2021-02-03 22:39:06 +0800

    line 9 edit by master

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("current branch is hot_fix");
         System.out.println("current branch is hot_fix 2");
+        System.out.println("current branch is master");
+        System.out.println("line 9 edit by master");
         System.out.println("current branch is hot_fix 3");
         System.out.println("line 9 edit by hot_fix");
     }
