4af1290eb3599cf7a138d3015e3603df4e7877d9
sunwy1995-2 <sunwy1995-2@gmail.com>
2021-02-04 09:42:55 +0800

    HelloGit2 created by hot fix

---
+++ HelloGit2/src/test/java/com/atguigu/git/TestHelloGit2.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+import org.junit.Test;
+
+/**
+ * @author WayneSun
+ * @create 2021-02-04 9:42
+ * @description
+ */
+public class TestHelloGit2 {
+    @Test
+    public void testHelloGit2(){
+        System.out.println("hot fix");
+    }
+}
